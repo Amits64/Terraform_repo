4f7cb7a06bff0aced336137c57dc7d1bcad5cd59
Amits64 <chauhanamit090@hotmail.com>
2024-08-14 21:53:01 +0000

    ECS Depoyment updated

--- ecs_ec2_cluster/modules/alb/main.tf
+++ ecs_ec2_cluster/modules/alb/main.tf
@@ -11,6 +11,7 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"  # Change target type to 'ip'
   health_check {
     path                = "/"
     interval            = 30
--- ecs_ec2_cluster/modules/vpc/main.tf
+++ ecs_ec2_cluster/modules/vpc/main.tf
@@ -4,6 +4,25 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_subnet" "public" {
   count             = 3
   vpc_id            = aws_vpc.main.id
